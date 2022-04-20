af13df59793a92e1d4941755e80f17acf90224df
Edward Nunez <edwardnunez31@gmail.com>
2022-04-20 10:03:35 -0400

    again

---
+++ 04.20.2022.js
@@ -0,0 +1,8 @@
+//Beginner - Lost Without a Map double the value in the array 
+function maps(x){
+    let newArr = []
+   for(let i = 0; i < x.length; i++){
+    newArr.push(x[i] * 2)
+  }
+    return newArr
+  }
