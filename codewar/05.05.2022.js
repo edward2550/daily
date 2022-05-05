94506af7b9271a85c2933221de7113a5fd0d1990
Edward Nunez <edwardnunez31@gmail.com>
2022-05-05 11:33:31 -0400

    equals on each side of an array

---
+++ codewar/05.05.2022.js
@@ -0,0 +1,17 @@
+function findEvenIndex(arr){
+  let equalIndex = -1
+  arr.forEach((item,index) => {
+      let leftSide = arr.slice(0,index).reduce((sum,item)=> sum + item,0)
+      let rightSide = slice(index + 1).reduce((sum,item)=> sum + item,0)
+      if(leftSide == rightSide) {equalIndex = index }
+      
+  })
+  return equalIndex
+}
+
+
+//loop through array and each something
+
+//reduce left and right of each index
+
+//get the subset array on each side of index  - slice 
