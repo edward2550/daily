1607d28e4580d5e8dc43e030568a071b35d9a9b6
Edward Nunez <edwardnunez31@gmail.com>
2022-05-05 08:47:30 -0400

    folder

--- object-practice/practice.js
+++ object-practice/practice.js
@@ -1,38 +1,65 @@
-function Stopwatch(){
-    let startTime , endTime, running , duration = 0
+// function Stopwatch(){
+//     let startTime , endTime, running , duration = 0
+
+//     this.start = function(){
+//         if(running){
+//             throw new Error('clock is arleady running')
+//         } else {
+//             running = true
+//             startTime = new Date()
+//         }
+//     }
+
+//     this.stop = function(){
+//         if(!running){
+//             throw new Error('clock is not running')
+//         } else {
+//             running = false 
+//             endTime = new Date()
+
+//             const seconds = (endTime.getTime() - startTime.getTime()) / 1000
+//             duration += seconds
+//         }
+//     }
+
+//     this.reset = function(){
+//         startTime = null;
+//         endTime = null;
+//         running = false;
+//         duration = 0;
+ 
+//     }
+//     Object.defineProperty(this,'duration',{
+//         get: function() {return duration}
+//     })
+
+// }
+
+// const sw = new Stopwatch()
+
+
+function StopWatch(){
+    let running, duration , startTime, endTime = 0
 
     this.start = function(){
         if(running){
-            throw new Error('clock is arleady running')
-        } else {
+            throw new Error('The clock is already running')
+        }
+        else{
             running = true
             startTime = new Date()
         }
     }
 
-    this.stop = function(){
+    this.stop(){
         if(!running){
-            throw new Error('clock is not running')
+            throw new Error('Stopwatch is not running')
         } else {
-            running = false 
+            running = false
             endTime = new Date()
-
-            const seconds = (endTime.getTime() - startTime.getTime()) / 1000
-            duration += seconds
+            duration = (endTime.getTime() - startTime.getTime()) / 1000
         }
-    }
 
-    this.reset = function(){
-        startTime = null;
-        endTime = null;
-        running = false;
-        duration = 0;
- 
     }
-    Object.defineProperty(this,'duration',{
-        get: function() {return duration}
-    })
 
 }
-
-const sw = new Stopwatch()
