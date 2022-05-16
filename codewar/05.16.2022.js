19d562cd9121a99acff2966ab036874d938e0e37
Edward Nunez <edwardnunez31@gmail.com>
2022-05-16 09:15:21 -0400

    XOXO

---
+++ codewar/05.16.2022.js
@@ -0,0 +1,14 @@
+//xo takes in a string whit letter , xo checks 
+function XO(str){
+    let x = 0;
+    let o = 0;
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i].toLowerCase() === 'x'){
+            x++
+        } else if(str[i].toLowerCase() === 'o'){
+            o++
+        }
+    }
+    return x === o;
+}
