80eb17f4f55b0db1e6eeb5b634aa7a63b191216a
Edward Nunez <edwardnunez31@gmail.com>
2022-06-07 08:54:47 -0400

    moving zero

---
+++ codewar/06.07.2022.js
@@ -0,0 +1,10 @@
+function moveZeros(arr) {
+    const zeros = arr.filter((elm) => elm === 0).length;
+   const result = arr.filter((elm) => elm !== 0);
+   
+   for (let i = 0; i < zeros; i += 1) {
+     result.push(0);
+   }
+   
+   return result;
+ }
