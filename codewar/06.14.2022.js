ed67592b01b24b8ad5d2e926bc84c6bfcc3d3b75
Edward Nunez <edwardnunez31@gmail.com>
2022-06-14 17:28:50 -0400

    Rot13

---
+++ codewar/06.14.2022.js
@@ -0,0 +1,4 @@
+function rot13(message){
+    const alpha = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+   return message.replace(/[a-z]/gi, letter => alpha[alpha.indexOf(letter) + 13]);
+ }
