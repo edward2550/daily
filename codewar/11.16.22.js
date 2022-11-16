ab0860e5d7871329850d87ba5b136d9090769b60
Edward Nunez <edwardnunez31@gmail.com>
2022-11-16 08:45:00 -0500

    hellow world

---
+++ codewar/11.16.22.js
@@ -0,0 +1,14 @@
+// Write a function "greet" that returns "hello world!"
+// write a function called "Greet" that returns hello worlds! - no arguments , return a string that said hello world
+function greet(){
+    return 'hello world!'
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  //greet() 'hello world'
