245802ae96518ca1feceb3597c7386e92da857e2
Edward Nunez <edwardnunez31@gmail.com>
2023-01-09 17:17:45 -0500

    daily

--- 01.06.23.js
+++ 01.06.23.js
@@ -1,6 +1,6 @@
 // No String or Array Methods (well brute force it first, but then no methods)! 
 //store all the leter in the string , increment the letter that appears the most 
-  //loop through the str check if the character exisit if it does add one to the value if it doesnt make it one
+  //loop through the str check if the character exisit if it does add one to the value if it doesnt make it one;;;;
 
   let charMap = {},
     count = 0,
